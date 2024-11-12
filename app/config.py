fd6ff4c2b51e9a7ed2671db0a4524a5d3b152553
Michael Studley <michael.r.studley@gmail.com>
2024-11-12 17:09:19 +0400

    Updated logic for env file location for both alembic and locally running

--- app/config.py
+++ app/config.py
@@ -1,17 +1,19 @@
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
 
 class Settings(BaseSettings):
     PG_URL: str
     REDIS_URL: str
+    ENV_FILE: str
+
+    if os.path.exists(".env"):
+        ENV_FILE = ".env"
+    else:
+        ENV_FILE = "../.env"
 
     model_config = SettingsConfigDict(
-        # Running from Pycharm
-        env_file="../.env",
-        # Running alembic commands
-        # env_file=".env",
+        env_file=ENV_FILE,
         extra="ignore",
     )
-
 
 Config = Settings()
